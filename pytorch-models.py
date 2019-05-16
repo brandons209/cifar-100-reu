7be2d98c5c08b77eb850423147d6d24353e0e539
brandons209 <brandons209@aol.com>
2019-05-16 13:52:04 -0400

    added num_classes param to vgg model

--- pytorch-models.py
+++ pytorch-models.py
@@ -12,12 +12,12 @@ cfg = {
 
 
 class VGG(nn.Module):
-    def __init__(self, vgg_name):
+    def __init__(self, vgg_name, num_classes):
 
         super(VGG, self).__init__()
         self.vgg_name = vgg_name
         self.features = self._make_layers(cfg[vgg_name])
-        self.classifier = nn.Linear(512, 10)
+        self.classifier = nn.Linear(512, num_classes)
 
 
     def forward(self, x):
