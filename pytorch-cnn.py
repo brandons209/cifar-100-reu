5592fd3e5f2395a6769135da0a609e7c36170aa6
brandons209 <brandons209@aol.com>
2019-05-17 12:54:27 -0400

    added validation and model saving

--- pytorch-cnn.py
+++ pytorch-cnn.py
@@ -21,6 +21,7 @@ from tensorboardX import SummaryWriter
 import os
 
 data_dir = "./data"
+checkpoint_path = "weights/pytorch_best_weights.pt"
 epochs = 50
 batch_size = 128
 num_classes = 100
@@ -64,9 +65,35 @@ if device == 'cuda':
 criterion = nn.CrossEntropyLoss()
 opt = optim.Adam(model.parameters())
 
+def test(epoch):
+    model.eval()
+    test_loss = 0
+    correct = 0
+    total = 0
+    for i, data in enumerate(test_loader):
+        inputs, targets = data
+        inputs, targets = inputs.to(device), targets.to(device)
+
+        outputs = model(inputs)
+        loss = criterion(outputs, targets)
+
+        test_loss += loss.item()
+        _, predicted = outputs.max(dim=1)
+        total += targets.size(0)
+        correct += predicted.eq(targets).sum().item()
+
+    if epoch == -1:
+        print("Test Results: loss: {:.4f}, acc: {:.2f}%".format(epoch + 1, test_loss/len(test_loader), (correct / total) * 100.0))
+    else:
+        print("Epoch [{}] Test: loss: {:.4f}, acc: {:.2f}%".format(epoch + 1, test_loss/len(test_loader), (correct / total) * 100.0))
+
+        return test_loss
+
+
 # Training phase
 model.train()
 print_step = len(train_loader) // 50
+best_loss = 0
 print("Training Starting...")
 for e in range(epochs):
     train_loss = 0
@@ -91,27 +118,18 @@ for e in range(epochs):
         correct += predicted.eq(targets).sum().item()
 
         if i % print_step == 0:
-            print("Epoch [{} / {}], Batch [{} / {}]: loss: {:.4f}, acc: {:.2f}".format(e+1, epochs, i+1, batch_size, train_loss/(i+1), (correct / total) * 100.0))
+            print("Epoch [{} / {}], Batch [{} / {}]: loss: {:.4f}, acc: {:.2f}%".format(e+1, epochs, i+1, len(train_loader), train_loss/(i+1), (correct / total) * 100.0))
 
-    print("Epoch [{} / {}]: loss: {:.4f}, acc: {:.2f}".format(e+1, epochs, train_loss/(len(train_loader)), (correct / total) * 100.0))
+    print("Epoch [{} / {}]: loss: {:.4f}, acc: {:.2f}%".format(e+1, epochs, train_loss/(len(train_loader)), (correct / total) * 100.0))
 
+    val_loss = test(e)
+    if e == 0:
+        best_loss = val_loss
+    elif val_loss < best_loss: # model improved
+        print('Saving Checkpoint..')
+        state = {'net': model.state_dict(), 'loss': val_loss, 'epoch': e}
+        torch.save(state, checkpoint_path)
+        best_loss = val_loss
 
 # Testing Phase
-model.eval()
-test_loss = 0
-correct = 0
-total = 0
-for i, data in test_loader:
-    inputs, targets = data
-    inputs, targets = inputs.to(device), targets.to(device)
-
-    outputs = model(inputs)
-    loss = criterion(outputs, targets)
-
-    test_loss += loss.item()
-    _, predicted = outputs.max(dim=1)
-    total += targets.size(0)
-    correct += predicted.eq(targets).sum().item()
-
-
-print("Testing Stats: loss: {:.4f}, acc: {:.2f}".format(test_loss/len(test_loader), (correct / total) * 100.0))
+test(-1)
